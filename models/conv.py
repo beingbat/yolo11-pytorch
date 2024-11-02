33fdf2f5b294112307e613c18eb0b4d33bf02fe3
beingbat <dev.arsalanmalek@gmail.com>
2024-11-02 15:34:00 +0500

    Populating certain params previously missed The inference would work now, plug n play

--- models/conv.py
+++ models/conv.py
@@ -20,7 +20,7 @@ class Conv(nn.Module):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
+        self.bn = nn.BatchNorm2d(c2, eps=0.001, momentum=0.03)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
     def forward(self, x):
